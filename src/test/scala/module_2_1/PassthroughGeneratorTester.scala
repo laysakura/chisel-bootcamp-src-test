821a170a95689da1aa007a60e7d1678117b96026
Sho Nakatani <lay.sakura@gmail.com>
2020-02-13 13:43:48 +0900

    wip

--- src/test/scala/module_2_1/PassthroughGeneratorTester.scala
+++ src/test/scala/module_2_1/PassthroughGeneratorTester.scala
@@ -1,32 +1,41 @@
 package module_2_1
 
+import chisel3.{iotesters, MultiIOModule}
 import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester}
-import chisel3.iotesters
 
 import scala.math.pow
 
-class PassthroughGeneratorTester extends ChiselFlatSpec {
-  private val backendNames =
-    if (firrtl.FileUtils.isCommandAvailable(Seq("verilator", "--version"))) {
+trait ChiselFlatSpecBase extends ChiselFlatSpec {
+  protected val backendNames: Array[String] =
+    if (firrtl.FileUtils.isCommandAvailable(Seq("verilator", "--version")))
       Array("firrtl", "verilator")
-    } else {
+    else
       Array("firrtl")
-    }
 
-  for (backendName <- backendNames) {
-    "PassthroughGenerator" should s"pass-through input (with $backendName)" in {
-      val width = 4
-      iotesters.Driver.execute(Array("--is-verbose",
-                                     "--backend-name",
-                                     backendName),
-                               () => new PassthroughGenerator(width)) { c =>
+  protected def test[T <: MultiIOModule](
+      cm: () => T,
+      testerGen: T => PeekPokeTester[T]): Unit =
+    backendNames.foreach { backendName =>
+      iotesters.Driver
+        .execute(Array("--is-verbose", "--backend-name", backendName), cm)(
+          testerGen) should be(true)
+    }
+}
+
+class PassthroughGeneratorTester extends ChiselFlatSpecBase {
+  "PassthroughGenerator" should "pass-through input" in {
+    val width = 4
+
+    test(
+      () => new PassthroughGenerator(width),
+      (c: PassthroughGenerator) => {
         new PeekPokeTester(c) {
           (0 until pow(2, width).toInt).foreach { v =>
             poke(c.io.in, v)
             expect(c.io.out, v)
           }
         }
-      } should be(true)
-    }
+      }
+    )
   }
 }
